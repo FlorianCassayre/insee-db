18dfe4e8aef85c01a313b7cee1dc1765ea450558
Florian Cassayre <florian.cassayre@gmail.com>
2020-02-18 14:48:07 +0100

    Place query parameter reading bug.

--- src/main/scala/db/file/writer/ShortHandler.scala
+++ src/main/scala/db/file/writer/ShortHandler.scala
@@ -6,5 +6,5 @@ import db.util.DatabaseUtils.ShortSize
 class ShortHandler extends DataHandler {
   override val Size: Int = ShortSize
   override def write(context: FileContextOut, value: Long): Unit = context.writeShort(value.toInt)
-  override def read(context: FileContextIn, offset: Long): Long = context.readShort(offset)
+  override def read(context: FileContextIn, offset: Long): Long = context.readShort(offset) & 0xffff
 }
